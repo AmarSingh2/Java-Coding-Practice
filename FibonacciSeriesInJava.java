229aabbc81bd9479bcf2817275c21fa90a7f0c9c
AmarSingh2 <114733083+AmarSingh2@users.noreply.github.com>
2024-01-27 23:25:25 +0530

    Add files via upload

---
+++ FibonacciSeriesInJava.java
@@ -0,0 +1,20 @@
+package com.selenium.practice;
+
+public class FibonacciSeriesInJava {
+
+	public static void main(String[] args) {
+
+		int a=0,b=1;
+		System.out.println(a+ " " +b);
+		
+		int c;
+		
+		for(int i=1;i<=10;i++) {
+			c=a+b;
+			System.out.print(" "+c);
+			a=b;
+			b=c;
+		}
+	}
+
+}
